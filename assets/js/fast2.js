fb2eb95ef35023375a54be22d0ebf729c5272c50
spiralgalaxytrs <80592602+spiralgalaxytrs@users.noreply.github.com>
2021-08-16 23:31:41 +0530

    Update fast2.js

--- assets/js/fast2.js
+++ assets/js/fast2.js
@@ -55,7 +55,7 @@ if(strText==""||strText1==""||strText2==""||strText3==""||strText4==""||strText6
 				localStorage.setItem("result2", strText3);
 				localStorage.setItem("result3", strText7);
 				localStorage.setItem("result4", strText8);
-				var frm = document.getElementsById('bookingForm')[0];
+				var frm = document.getElementById('bookingForm')[0];
 				frm.reset();
 				window.location.href = "bookingSuccess.html";
 			}
